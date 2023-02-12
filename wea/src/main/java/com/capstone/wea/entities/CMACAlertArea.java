20f5729a2d1f24d4aa05af7804f7053ebcd09095
dargall <dargall91@comcast.net>
2023-02-12 12:51:34 -0500

    Continue to define entities

---
+++ wea/src/main/java/com/capstone/wea/entities/CMACAlertArea.java
@@ -0,0 +1,38 @@
+package com.capstone.wea.entities;
+
+import com.capstone.wea.model.cmac.CMACCapGeocodeModel;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JacksonXmlRootElement(localName = "CMAC_Alert_Area")
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class CMACAlertArea {
+    @Id
+    private int messageNumber;
+
+    @JsonProperty("CMAC_area_description")
+    private String areaDescription;
+
+    @JsonProperty("CMAC_polygon")
+    private String polygon;
+
+    @JsonProperty("CMAC_circle")
+    private String circle;
+
+    @JsonProperty("CMAC_cmas_geocode")
+    @JacksonXmlElementWrapper(useWrapping = false)
+    private List<String> geocodeList;
+    @JsonProperty("CMAC_cap_geocode")
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name="messageNumber")
+    private List<CMACCapGeocodeModel> capGeocodeList;
+}
