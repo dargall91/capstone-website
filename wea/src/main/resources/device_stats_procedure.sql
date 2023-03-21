06f99dd0d8fe016c8ace2f5b1d810862dd16df7f
dargall <dargall91@comcast.net>
2023-03-21 19:23:03 -0400

    Update message stats query

--- wea/src/main/resources/device_stats_procedure.sql
+++ wea/src/main/resources/device_stats_procedure.sql
@@ -12,19 +12,45 @@ BEGIN
 	DECLARE offsetVal INT DEFAULT 9 * (IF(pageNum < 1, 1, pageNum) - 1);
 
 	SELECT CMACMessage.messageNumber, COUNT(*) AS deviceCount,
-	CAST(SEC_TO_TIME(AVG(TIME_TO_SEC(TIMEDIFF(timeReceived, sentDateTime)))) AS TIME) AS averageTime,
-	MIN(TIMEDIFF(timeReceived, sentDateTime)) AS shortestTime,
+    CAST(SEC_TO_TIME(AVG(TIME_TO_SEC(TIMEDIFF(timeReceived, sentDateTime)))) AS TIME) AS averageTime,
+    MIN(TIMEDIFF(timeReceived, sentDateTime)) AS shortestTime,
     MIN(timeReceived) AS firstReceived,
     MIN(timeDisplayed) AS firstDisplayed,
-	CAST(SEC_TO_TIME(AVG(TIME_TO_SEC(TIMEDIFF(timeDisplayed, timeReceived)))) AS TIME) AS averageDisplayDelay,
-	SUM(NOT receivedInside) AS receivedOutside,
-	SUM(NOT displayedInside) AS displayedOutside,
-    AVG((SELECT SUM(distanceFromPolygon) FROM CollectedDeviceData WHERE CollectedDeviceData.messageNumber = IFNULL(messageNumber, CMACMessage.messageNumber) AND messagePresented IS TRUE)) as averageDistance,
-    (SELECT MIN(distanceFromPolygon) FROM CollectedDeviceData WHERE CollectedDeviceData.messageNumber = IFNULL(messageNumber, CMACMessage.messageNumber) AND messagePresented IS TRUE) as minDistance,
-    (SELECT MAX(distanceFromPolygon) FROM CollectedDeviceData WHERE CollectedDeviceData.messageNumber = IFNULL(messageNumber, CMACMessage.messageNumber) AND messagePresented IS TRUE) as maxDistance,
-    SUM(messagePresented IS TRUE) as presented
-	FROM CollectedDeviceData JOIN CMACMessage
-	ON CMACMessage.messageNumber = CollectedDeviceData.messageNumber
+    CAST(SEC_TO_TIME(AVG(TIME_TO_SEC(TIMEDIFF(timeDisplayed, timeReceived)))) AS TIME) AS averageDisplayDelay,
+    SUM(NOT receivedInside) AS receivedOutside,
+    SUM(NOT receivedInside AND NOT messagePresented) AS notDisplayedOutside,
+    SUM(NOT messagePresented AND optedOut) as optedOut,
+    SUM(messagePresented AND NOT locationAvailable) as displayedDefault,
+    SUM(messagePresented IS TRUE) AS presented,
+    (
+    	SELECT AVG(distanceFromPolygon) as distance
+    	FROM CollectedDeviceData
+    	WHERE CollectedDeviceData.messageNumber = IFNULL(messageNumber, CMACMessage.messageNumber)
+    	AND messagePresented IS TRUE
+    ) AS averageDistance,
+    (
+    	SELECT AVG(distanceFromPolygon)
+    	FROM
+    	(
+    		SELECT distanceFromPolygon, ROW_NUMBER() OVER (ORDER BY distanceFromPolygon) AS RowNumber, COUNT(*) OVER () AS Count
+    		FROM CollectedDeviceData
+    		WHERE CollectedDeviceData.messageNumber = IFNULL(messageNumber, CMACMessage.messageNumber)
+    		AND messagePresented IS TRUE
+    	) AS median
+    	WHERE 2 * RowNumber IN (Count, Count + 1, Count + 2)
+    ) AS medianDistance,
+    (
+    	SELECT MIN(distanceFromPolygon)
+    	FROM CollectedDeviceData
+        WHERE CollectedDeviceData.messageNumber = IFNULL(messageNumber, CMACMessage.messageNumber)
+    ) AS minDistance,
+    (
+    	SELECT MAX(distanceFromPolygon)
+    	FROM CollectedDeviceData
+    	WHERE CollectedDeviceData.messageNumber = IFNULL(messageNumber, CMACMessage.messageNumber)
+    ) AS maxDistance
+    FROM CollectedDeviceData JOIN CMACMessage
+    ON CMACMessage.messageNumber = CollectedDeviceData.messageNumber
     WHERE CMACMessage.sender = sender
     AND CMACMessage.messageNumber = IFNULL(messageNumber, CMACMessage.messageNumber)
     AND CMACMessage.messageType = IFNULL(messageType, CMACMessage.messageType)
